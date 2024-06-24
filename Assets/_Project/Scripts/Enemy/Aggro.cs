88771c65e64e35e68d3f66c8e7dc5f7fc7770ff4
TheOctan <kanatush.tim@gmail.com>
2024-06-24 12:49:12 +0300

    Cool down

--- Assets/_Project/Scripts/Enemy/Aggro.cs
+++ Assets/_Project/Scripts/Enemy/Aggro.cs
@@ -1,11 +1,19 @@
+using System.Collections;
 using UnityEngine;
 
 namespace OctanGames.Enemy
 {
     public class Aggro : MonoBehaviour
     {
+        [Header("Properties")]
+        [SerializeField] private float _coolDown;
+
+        [Header("Components")]
         [SerializeField] private TriggerObserver _triggerObserver;
         [SerializeField] private AgentMoveToPlayer _follow;
+
+        private Coroutine _agroCoroutine;
+        private bool _hasAgroTarget;
 
         private void Start()
         {
@@ -21,8 +29,37 @@ namespace OctanGames.Enemy
             _triggerObserver.TriggerExit -= TriggerExit;
         }
 
-        private void TriggerEnter(Collider obj) => SwitchFollowOn();
-        private void TriggerExit(Collider obj) => SwitchFollowOff();
+        private void TriggerEnter(Collider obj)
+        {
+            StopAgroCoroutine();
+            SwitchFollowOn();
+        }
+
+        private void TriggerExit(Collider obj)
+        {
+            if (_hasAgroTarget) return;
+            _hasAgroTarget = true;
+
+            _agroCoroutine = StartCoroutine(SwitchFollowOffAfterCoolDown());
+        }
+
+        private void StopAgroCoroutine()
+        {
+            if (!_hasAgroTarget) return;
+            _hasAgroTarget = false;
+
+            if (_agroCoroutine == null) return;
+
+            StopCoroutine(_agroCoroutine);
+            _agroCoroutine = null;
+        }
+
+        private IEnumerator SwitchFollowOffAfterCoolDown()
+        {
+            yield return new WaitForSeconds(_coolDown);
+            SwitchFollowOff();
+        }
+
         private void SwitchFollowOn() => _follow.enabled = true;
         private void SwitchFollowOff() => _follow.enabled = false;
     }
