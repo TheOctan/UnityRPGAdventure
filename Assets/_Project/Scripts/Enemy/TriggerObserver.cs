2ba65ebf2ad68e9831ce38d533a71a6b3a356726
TheOctan <kanatush.tim@gmail.com>
2024-06-24 12:15:37 +0300

    Trigger observer

---
+++ Assets/_Project/Scripts/Enemy/TriggerObserver.cs
@@ -0,0 +1,15 @@
+using System;
+using UnityEngine;
+
+namespace OctanGames.Enemy
+{
+    [RequireComponent(typeof(Collider))]
+    public class TriggerObserver : MonoBehaviour
+    {
+        public event Action<Collider> TriggerEnter;
+        public event Action<Collider> TriggerExit;
+
+        private void OnTriggerEnter(Collider other) => TriggerEnter?.Invoke(other);
+        private void OnTriggerExit(Collider other) => TriggerExit?.Invoke(other);
+    }
+}
