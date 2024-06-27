e5b1afdaed1cdb07f7150f58def1040705154f80
TheOctan <kanatush.tim@gmail.com>
2024-06-27 11:19:11 +0300

    Hero death

---
+++ Assets/_Project/Scripts/Hero/HeroDeath.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+namespace OctanGames.Hero
+{
+    [RequireComponent(typeof(HeroHealth))]
+    public class HeroDeath : MonoBehaviour
+    {
+        [SerializeField] private HeroHealth _health;
+        [SerializeField] private HeroMove _heroMove;
+        [SerializeField] private HeroAnimator _heroAnimator;
+        [SerializeField] private GameObject _deathFX;
+
+        private bool _isDead;
+
+        private void Start() => _health.HealthChanged += OnHealthChanged;
+        private void OnDestroy() => _health.HealthChanged -= OnHealthChanged;
+
+        private void OnHealthChanged()
+        {
+            if (_isDead || _health.Current > 0) return;
+
+            Die();
+        }
+
+        private void Die()
+        {
+            _isDead = true;
+            
+            _heroMove.enabled = false;
+            _heroAnimator.PlayDeath();
+
+            Instantiate(_deathFX, transform.position, Quaternion.identity);
+        }
+    }
+}
