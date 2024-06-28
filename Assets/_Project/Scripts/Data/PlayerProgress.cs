e3b6efab35d7ee4c4e3d56d49d58f225b0d7dec6
TheOctan <kanatush.tim@gmail.com>
2024-06-28 16:06:31 +0300

    Spawner slain state

--- Assets/_Project/Scripts/Data/PlayerProgress.cs
+++ Assets/_Project/Scripts/Data/PlayerProgress.cs
@@ -8,12 +8,14 @@ namespace OctanGames.Data
         public WorldData WorldData;
         public PlayerState PlayerState;
         public Stats PlayerStats;
+        public KillData KillData;
 
         public PlayerProgress(string initialLevel)
         {
             WorldData = new WorldData(initialLevel);
             PlayerState = new PlayerState();
             PlayerStats = new Stats();
+            KillData = new KillData();
         }
     }
 }
--- Assets/_Project/Scripts/Logic/EnemySpawner.cs
+++ Assets/_Project/Scripts/Logic/EnemySpawner.cs
@@ -1,14 +1,42 @@
-using System;
+using OctanGames.Data;
+using OctanGames.Infrastructure.Services.PersistentProgress;
 using OctanGames.StaticData;
 using UnityEngine;
 
 namespace OctanGames.Logic
 {
-    public class EnemySpawner : MonoBehaviour
+    public class EnemySpawner : MonoBehaviour, ISavedProgressWriter
     {
         [SerializeField] private MonsterType _monsterTypeId;
 
-        private void OnDrawGizmos()
+        private string _id;
+        [SerializeField] private bool _slain;
+
+        private void Awake()
+        {
+            _id = GetComponent<UniqueId>().Id;
+        }
+
+        void ISavedProgressReader.LoadProgress(PlayerProgress progress)
+        {
+            if (progress.KillData.ClearedSpawners.Contains(_id))
+            {
+                _slain = true;
+            }
+            else
+            {
+                Spawn();
+            }
+        }
+
+        void ISavedProgressWriter.SaveProgress(PlayerProgress progress)
+        {
+            if (!_slain) return;
+
+            progress.KillData.ClearedSpawners.Add(_id);
+        }
+
+        private void Spawn()
         {
             
         }
