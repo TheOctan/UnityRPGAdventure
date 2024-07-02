46bf5242227621109c8690beb081af800263adfd
TheOctan <kanatush.tim@gmail.com>
2024-07-02 13:25:52 +0300

    Pickup fx

--- Assets/_Project/Scripts/Enemy/LootPiece.cs
+++ Assets/_Project/Scripts/Enemy/LootPiece.cs
@@ -1,10 +1,17 @@
+using System.Collections;
 using OctanGames.Data;
+using TMPro;
 using UnityEngine;
 
 namespace OctanGames.Enemy
 {
     public class LootPiece : MonoBehaviour
     {
+        [SerializeField] private GameObject _skull;
+        [SerializeField] private GameObject _pickupFxPrefab;
+        [SerializeField] private TextMeshPro _lootText;
+        [SerializeField] private GameObject _pickupPopup;
+
         private WorldData _worldData;
         private Loot _loot;
         private bool _picked;
@@ -18,7 +25,29 @@ namespace OctanGames.Enemy
             if(_picked) return;
             _picked = true;
 
-            _worldData.LootData.Collect(_loot);
+            UpdateWorldData();
+
+            HideSkull();
+            PlayPickupFx();
+            ShowText();
+
+            StartCoroutine(StartDestroyTimer());
+        }
+
+        private void UpdateWorldData() => _worldData.LootData.Collect(_loot);
+        private void HideSkull() => _skull.SetActive(false);
+        private void PlayPickupFx() => Instantiate(_pickupFxPrefab, transform.position, Quaternion.identity);
+
+        private void ShowText()
+        {
+            _lootText.text = _loot.Value.ToString();
+            _pickupPopup.SetActive(true);
+        }
+
+        private IEnumerator StartDestroyTimer()
+        {
+            yield return new WaitForSeconds(1.5f);
+            Destroy(gameObject);
         }
     }
 }
